a8c054d2354b111e77a98612505bec5b065bfe37
Iftakher Hossen <you@example.comiftakher.one@gmail.com>
2024-06-27 12:50:17 +0600

    csrf origins added

--- library_management/settings.py
+++ library_management/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['https://library-management-voja.onrender.com', 'https://*.127.0.0.1']
 
 # Application definition
 
